344c3a741999f6caa0ae53ad7810098a7962ae8d
Ievgenii <bijibnui88@gmail.com>
2020-02-19 20:31:41 +0200

    build/

---
+++ build/js/scripts.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function myFunction() {
+  var x = document.getElementById("overlay");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction02() {
+  var x = document.getElementById("overlay");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
